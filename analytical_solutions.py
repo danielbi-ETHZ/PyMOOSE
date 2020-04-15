5cb033bfbf94a8430fc69a3023ebe81a73b145d0
Daniel Birdsell <danielbi@ethz.ch>
2020-04-15 16:06:59 +0200

    function to extract data from [Postprocessors] to plot disp(t) at a point

--- analytical_solutions.py
+++ analytical_solutions.py
@@ -1,7 +1,8 @@
 def theis_solution(permeability_val=None,density_val=None,viscosity_val=None,
                    filename=None,use_mobility=None,
                    flux_function = None, b = None, well_area = None, Kw = None,
-                   phi = None, t_end = None):
+                   phi = None, t_end = None, z_plot_loc = None, fixed_axis = None,
+                   pp_loc = None,P0 = None, symmetry = True):
     import numpy as np
     import matplotlib.pyplot as plt
     import mpmath
@@ -26,6 +27,14 @@ def theis_solution(permeability_val=None,density_val=None,viscosity_val=None,
         t_end = 100.0; #seconds
     if use_mobility == None:
         use_mobility = False
+    if z_plot_loc == None:
+        z_plot_loc = -199.77360637445
+    if fixed_axis == None:
+        fixed_axis = 2 # integer telling which axis should be fixed (x = 0, y = 1, z = 2)
+    if pp_loc == None:
+        pp_loc = -1 #integer to tell where the pore pressure is found in csv file
+    if P0 == None:
+        P0 = 0.
     if well_area == None:
         dx = 1.07131; dy = 1.0; #meters
         well_area = dx*dy; # well area (m2)
@@ -47,11 +56,14 @@ def theis_solution(permeability_val=None,density_val=None,viscosity_val=None,
     print 'volumetric flow rate (checked against Theis)', Q
 
     ## Calculate the analytical solution my way
-    prefactor = 4.0*Q*viscosity_val/(4.0*np.pi*b*permeability_val) # Pa, multiplier of exponential integral, 4.0 factor is because of 1/4 grid symmetry
+    import pudb; pudb.set_trace()
+    if symmetry:
+        prefactor = 4.0*Q*viscosity_val/(4.0*np.pi*b*permeability_val) # Pa, multiplier of exponential integral, 4.0 factor is because of 1/4 grid symmetry
+    else:
+        prefactor = Q*viscosity_val/(4.0*np.pi*b*permeability_val) # Pa, multiplier of exponential integral, 4.0 factor is because of 1/4 grid symmetry
     r_anal = np.linspace(1,200,101); #radius in meters
     dP_anal = np.zeros(len(r_anal))
-    t_final = 1.e2; # seconds
-    u = (r_anal**2.0)*phi*viscosity_val/(4.0*permeability_val*t_final*Kw)
+    u = (r_anal**2.0)*phi*viscosity_val/(4.0*permeability_val*t_end*Kw)
     for i in xrange(0,len(u)):
         dP_anal[i] = prefactor*mpmath.e1(u[i])
 
@@ -69,12 +81,74 @@ def theis_solution(permeability_val=None,density_val=None,viscosity_val=None,
                 junkvar = 1
                 line_count += 1
             else:
-                if float(row[2]) > -199.77360637445-.1 and float(row[2]) <  -199.77360637445+.1:
+                if float(row[fixed_axis]) > z_plot_loc-.1 and float(row[fixed_axis]) <  z_plot_loc+.1 and float(row[0])>0:
                     x[line_count-1] = float(row[0])
-                    pp[line_count-1] = float(row[-1])
+                    pp[line_count-1] = float(row[pp_loc])-P0
                     line_count += 1
                 # x = np.append([x],[row[0]]);
                 # pp = np.append([pp],[row[-1]]);
 
 
     plt.plot(x,pp,'.')
+    # plt.savefig('theis_solution.png')
+
+def porosity_weighted_average(val=None,phi=None):
+    """
+    This function takes the fluid and solid value and calculates an average
+    in the way presented in the MOOSE documentation and Saar (2011) review.
+    Useful for calculating the average thermal conductivity and other thermal
+    parameters.
+
+    val [val_f,val_s] should be lenght two with the value of the fluid and solid
+    phi is the porosity
+    """
+    import numpy as np
+
+    if val is None:
+        print('ERROR, need provide val to provide fluid and solid values')
+        alkjfsdlk #error
+    elif len(val) != 2:
+        print('ERROR, need the value to provide fluid and solid values')
+        lkjalksdfj #error
+
+    if phi is None:
+        phi = 0.1
+        print('WARNING: val_f not defined')
+
+    weighted_average = float(phi)*float(val[0]) + (1.0-float(phi))*float(val[1])
+    return weighted_average
+
+
+def Postprocessors_plot_over_time(filename = None):
+    """
+    This plots the values over time from a [Posptroccessor] with
+    type = PointValue and outputs = csv.
+    """
+    import csv
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import mpmath
+    import os
+
+    t = np.zeros(0)
+    uz_0 = np.zeros(0);
+    uz_1 = np.zeros(0);
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                junkvar = 1
+                line_count += 1
+            else:
+                t = np.append(t,float(row[0]))
+                uz_0 = np.append(uz_0,float(row[1]))
+                uz_1 = np.append(uz_1,float(row[2]))
+                # if float(row[fixed_axis]) > z_plot_loc-.1 and float(row[fixed_axis]) <  z_plot_loc+.1 and float(row[0])>0:
+                #     x[line_count-1] = float(row[0])
+                #     pp[line_count-1] = float(row[pp_loc])-P0
+                #     line_count += 1
+                # x = np.append([x],[row[0]]);
+                # pp = np.append([pp],[row[-1]]);
+
+    return [t, uz_0, uz_1]
